bed4081960605b00cb9a44b61d453295870665b5
navinkumar.d <navinkumar.d@diatoz.com>
2025-01-27 08:01:05 +0530

    subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DiUserService } from './DI/di-user.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,29 +10,71 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 
 export class AppComponent {
-  users: { name: string, status: string, id?: number }[] = []
+  observableData1: any
+  subjectData1: any
+  observableData2: any
+  subjectData2: any
 
-  constructor(private userService: DiUserService, private router: Router, private activeRouter: ActivatedRoute) { }
+  myData: any[] = []
+  // subject = new Subject();
+  // subject = new ReplaySubject(2); // memroy data
+  subject = new BehaviorSubject(0); //initial value
 
-  ngOnInit(): void {
-    this.users = this.userService.users
-    console.log(this.users);
+
+  getObservableData() {
+    console.log("getObservableData");
+
+    let myObserable = new Observable<any>(observer => {
+      // observer.next("data emited from the myObserable")
+      observer.next(Math.floor(Math.random() * 99) + 1)
+    })
+
+    myObserable.subscribe(data => {
+      this.observableData1 = data
+    })
+    myObserable.subscribe(data => {
+      this.observableData2 = data
+    })
+  }
+  getSubjectData() {
+    console.log("getSubjectData");
+
+    let mySubject = new Subject()
+
+    mySubject.subscribe(data => {
+      this.subjectData1 = data
+    })
+    mySubject.subscribe(data => {
+      this.subjectData2 = data
+    })
+    // mySubject.next("data emited from the mySubject");
+    mySubject.next(Math.floor(Math.random() * 99) + 1)
   }
 
-  getStatus(status: string) {
-    console.log(status, "from getStatus fun");
+  emitData() {
+    console.log("emitData");
+    this.subject.next(1);
+    setTimeout(() => {
+      this.subject.next(2);
+    }, 3000);
+    setTimeout(() => {
+      this.subject.next(3);
+    }, 6000);
+    setTimeout(() => {
+      this.subject.next(4);
+    }, 9000);
+    setTimeout(() => {
+      this.subject.next(5);
+    }, 1200);
+  }
+  getData() {
+    this.subject.subscribe(data => {
+      console.log("getData", data);
+      this.myData.push(data)
+    })
   }
 
-  goToPage(route: string, queryParams: string, params?: number) {
-    if (queryParams) {
-      this.router.navigate([route], { queryParams: { userName: queryParams } })
-    } else if (params) {
-      this.router.navigate([route, params])
-    } else {
-      this.router.navigate([route], { relativeTo: this.activeRouter })
-
-      // absolute path
-      // this.router.navigateByUrl(`/pipe/${id}?name=${name}`)
-    }
+  getValue(event: any) {
+    console.log(event?.target?.value);
   }
 }
