f5dbc6d5b6c76f321cf8203e387f30fc219016df
navinkumar.d <navinkumar.d@diatoz.com>
2025-01-31 08:07:13 +0530

    api handling

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms'
+import { CrudService } from 'src/app/services/crud.service';
 @Component({
   selector: 'besant-signup',
   templateUrl: './signup.component.html',
@@ -10,11 +11,15 @@ export class SignupComponent implements OnInit {
 
   userName: string = ""
 
+  users: any[] = [];
+
+  isEdit: boolean = false
+
   registrationForm!: FormGroup;
 
   // userEmail = new FormControl('', Validators.required);
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private crudService: CrudService) { }
 
   ngOnInit(): void {
     console.log("SignupComponent component loaded");
@@ -40,6 +45,7 @@ export class SignupComponent implements OnInit {
     // });
 
     this.registrationForm = this.fb.group({
+      id: [''],
       fullName: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
@@ -47,27 +53,96 @@ export class SignupComponent implements OnInit {
       gender: ['', Validators.required],
       address: ['', Validators.required],
       dob: ['', Validators.required],
-      profilePicture: ['', Validators.required],
+      // profilePicture: ['', Validators.required],
       terms: ['', Validators.requiredTrue],
     });
 
 
-    this.registrationForm.valueChanges.subscribe({
-      next: (data) => {
-        console.log(this.registrationForm, "registrationForm value changes");
-        // if (data.phone == 123) {
-        //   this.registrationForm.controls['email'].setErrors(null)
-        //   this.registrationForm.controls['address'].setErrors({ 'incorrect': true })
-        // }
-      }
-    })
+    // this.registrationForm.valueChanges.subscribe({
+    //   next: (data) => {
+    //     console.log(this.registrationForm, "registrationForm value changes");
+    //     // if (data.phone == 123) {
+    //     //   this.registrationForm.controls['email'].setErrors(null)
+    //     //   this.registrationForm.controls['address'].setErrors({ 'incorrect': true })
+    //     // }
+    //   }
+    // })
+
+    this.getUsers()
 
   }
 
   onSubmit() {
-    this.registrationForm.valid ? console.log("form is submited", this.registrationForm.value) : console.log("form is submited  and invalid")
+    if (!this.registrationForm.valid) return
+    const user = this.registrationForm.value;
+    console.log(user, "userss", this.isEdit);
 
-    this.registrationForm.markAllAsTouched()
+    if (this.isEdit) {
+      this.crudService.updateUser(user.id, user).subscribe({
+        next: (data: any) => {
+          console.log(data, "create users api success response");
+          this.getUsers()
+        }
+      })
+    } else {
+      this.crudService.createUser(user).subscribe({
+        next: (data: any) => {
+          console.log(data, "create users api success response");
+          this.getUsers()
+        }
+      })
+    }
+
+
+  }
+
+  getUsers() {
+    this.crudService.getUsers().subscribe({
+      next: (users) => {
+        console.log(users, "coming from backend");
+        this.users = users
+        this.isEdit = false
+        this.registrationForm.reset()
+      }
+
+    })
+  }
+
+  editUser(user: any) {
+    console.log(user, "editUser");
+    this.isEdit = true
+    const { fullName, email, password, phone, gender, address, dob, profilePicture, terms, id } = user;
+    // this.registrationForm.patchValue({
+    //   fullName,
+    //   email,
+    //   // password,
+    //   // phone,
+    //   // gender,
+    //   // address,
+    //   // dob,
+    //   // id,
+    //   // terms,
+    // })
+    this.registrationForm.setValue({
+      fullName,
+      email,
+      password,
+      phone,
+      gender,
+      address,
+      dob,
+      id,
+      terms,
+    })
+  }
+  deleteUser(user: any) {
+    this.crudService.deletedUser(user.id).subscribe({
+      next: (response: any) => {
+        console.log(response, "response");
+        this.getUsers()
+
+      }
+    })
   }
 
 }
--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,23 +7,48 @@ import { Observable } from 'rxjs';
 })
 export class CrudService {
 
+  API_URL: string = "https://679c2a8a33d3168463260fc2.mockapi.io/user";
+
   constructor(private http: HttpClient) { }
 
 
   createUser(user: any): Observable<any[]> {
-    return this.http.post<any>("url", user)
+    return this.http.post<any>(this.API_URL, user, {
+      headers: new HttpHeaders({
+        'token': "secret message to get response"
+      })
+    })
   }
 
   getUsers(): Observable<any[]> {
-    return this.http.get<any>("url")
+    return this.http.get<any>(this.API_URL, {
+      headers: new HttpHeaders({
+        'token': "secret message to get response"
+      })
+    })
   }
 
-  updateUser(id: number): any {
-    return this.http.put<any>("url/2", { fullName: "naveenkumar" })
+  updateUser(id: number, user: any): any {
+    console.log({ id, user }, "payload of update");
+
+    return this.http.put<any>(`${this.API_URL}/${id}`, user, {
+      headers: new HttpHeaders({
+        'token': "secret message to get response"
+      })
+    })
   }
 
   deletedUser(id: number): any {
-    return this.http.delete<any>("url")
+    return this.http.delete<any>(`${this.API_URL}/${id}`, {
+      headers: new HttpHeaders({
+        'token': "secret message to get response"
+      })
+    })
   }
 
 }
+
+// interceptors
+// error handling in api
+// auth
+// modules
